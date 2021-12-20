9937b54bc5873d9ec2209513be1d5308522edceb
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-20 21:37:44 +0530

    added utf

---
+++ Utf.java
@@ -0,0 +1,12 @@
+class Utf{
+		static String paddi="ehebfdhfhfuwrfhuwsdhfjdjhsdf";
+		static String[] maxi={"deasd","dedff","rfget","wdwefw"};
+		static String[] yuzi={"deafr","rjrjj","edee","edqwda"};
+	public static void main(String[] args){
+		System.out.println(yuzi[3]);
+		System.out.println(paddi);
+		System.out.println(maxi[2]);
+	}
+
+	
+}
